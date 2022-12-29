c44a4cfd1920816f8e5999e2b8617d84eb6d6765
MarcosAlberto21 <3004818880101@ingenieria.usac.edu.gt>
2022-12-28 20:03:10 -0600

    [ADD] AGREGAR ANALISIS SEMANTICO DE EXPRESIONES ARIMETICAS

--- BACKEND/services/analisis/servicesAnalisis.js
+++ BACKEND/services/analisis/servicesAnalisis.js
@@ -13,6 +13,7 @@ router.post("/Analizador", async (req, res) => {
     let raiz = parser.parse(req.body.text);
 
     recorrer_arbol(raiz);
+    recorrer_arbol_para_analisis(raiz);
 
     // res.send({errores_sintacticos:resultado.getErrores_sintacticos(), tabla_simbolos: resultado.getSimbolos()});
     res.send({raiz:salida_dot})
@@ -29,12 +30,71 @@ function recorrer_arbol(nodo){
 
   salida_dot = salida_dot + `${nodo.id}[label= "${nodo.valor}" fillcolor="#d62728" shape="circle"];`
 
-
   nodo.hijos.forEach(element => {
     salida_dot = salida_dot + `${nodo.id} -> ${id_n} ;`
     recorrer_arbol(element)
   });
 
+}
+
+function recorrer_arbol_para_analisis(nodo){
+
+
+  if(nodo.valor == 'LEXPRESION'){
+    console.log("Encontré una lista de expresiones");
+  }else if(nodo.valor == 'EXPRESION'){
+    console.log("tengo que analizar esta expresion");
+    var calculo = analizar_expresion(nodo.hijos[0]);
+    console.log("El resulta de la expresion es: " + calculo);
+  
+  }
+
+
+  nodo.hijos.forEach(element => {
+    // salida_dot = salida_dot + `${nodo.id} -> ${id_n} ;`
+    recorrer_arbol_para_analisis(element)
+  });
+
+
+}
+
+function analizar_expresion(expresion){
+  console.log("lista para analizar",expresion.valor);
+
+  var respuesta = null;
+
+  switch (expresion.tipo) {
+    case "suma":
+        respuesta = parseFloat(analizar_expresion(expresion.hijos[0]) +  analizar_expresion(expresion.hijos[1]));
+
+      break;
+      case "resta":
+        respuesta = parseFloat(analizar_expresion(expresion.hijos[0]) -  analizar_expresion(expresion.hijos[1]));
+      
+      break;
+      case "division":
+        respuesta = parseFloat(analizar_expresion(expresion.hijos[0]) /  analizar_expresion(expresion.hijos[1]));
+      
+      break;
+      case "multiplicar":
+        respuesta = parseFloat(analizar_expresion(expresion.hijos[0]) *  analizar_expresion(expresion.hijos[1]));
+
+      break;
+
+      case "decimal":
+        respuesta = parseFloat(expresion.valor)
+      break;
+
+      case "id":
+        //anda busca a mi tabla de simbolos
+          console.log("aqui va un identificador que esta asociado a un valor");
+        break;
+    default:
+      console.log("Error semantico en operadores");
+      break;
+  }
+
+  return respuesta;
 }
 
 
